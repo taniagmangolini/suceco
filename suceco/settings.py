f46417316d5c3c6c95a5c91126877d105b63a61b
taniagmangolini <taniagmangolini@gmail.com>
2020-10-10 19:41:36 -0300

    ajustes

--- suceco/settings.py
+++ suceco/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5h*m=ynom!iv2zh-27@0lrev80ws4di4teb&6u+@damvijryuw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
