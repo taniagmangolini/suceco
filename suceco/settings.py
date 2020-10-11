59b46adbe6655f8d8226dbb5cc6c97a2a2690906
taniagmangolini <taniagmangolini@gmail.com>
2020-10-10 21:39:26 -0300

    ajustes

--- suceco/settings.py
+++ suceco/settings.py
@@ -77,21 +77,23 @@ WSGI_APPLICATION = 'suceco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+  #  'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+  #  }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    # 'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': os.environ.get('DB_NAME', 'nome_do_banco'),
-    #    'USER': os.environ.get('DB_USER', 'nome_do_user'),
-    #    'PASSWORD': os.environ.get('DB_PASS', 'senha_do_user'),
-    #    'HOST': 'localhost',
-    #    'PORT': '5432',
-    #}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'gfvqvdbbjfaccg',
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': 'ec2-3-228-114-251.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
