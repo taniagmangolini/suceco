bc56177f9a6d3ae95e7e2ad9aa6ab99a17fe2e33
taniagmangolini <taniagmangolini@gmail.com>
2020-10-14 00:06:52 -0300

    ajustes

--- suceco/settings.py
+++ suceco/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'local'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'local'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'suceco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
+'''
 DATABASES = {
   'default': {
       'ENGINE': 'django.db.backends.sqlite3',
@@ -97,7 +97,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
