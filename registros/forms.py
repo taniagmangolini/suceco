c4d575fe7bef5e943447cbe8852b7a0f34ce6b0a
taniagmangolini <taniagmangolini@gmail.com>
2020-10-13 22:08:03 -0300

    ajustes

--- registros/forms.py
+++ registros/forms.py
@@ -7,7 +7,6 @@ from django.forms import ModelChoiceField
 class RegistroForm(forms.ModelForm):
     class Meta:
         model = Registro
-        fields = '__all__'
         help_texts = {'id': ('Id'),
                       'especie' : ('Espécie'),
                       'estagio' : ('Estágio Sucessional'),
@@ -22,10 +21,9 @@ class RegistroForm(forms.ModelForm):
         def __init__(self, *args, **kwargs) :
             super(RegistroForm, self).__init__(*args, **kwargs)
 
-            self.fields['formacao_florestal'] = ModelChoiceField(queryset=sorted(FormacaoFlorestal.objects.all(),
-                                                                 key=lambda x : x.nome),
+            self.fields['formacao_florestal'] = ModelChoiceField(queryset=FormacaoFlorestal.objects.all().order_by('nome'),
                                                                  to_field_name='nome',
                                                                  empty_label="Selecione a Formação Florestal")
-            self.fields['especie'] = ModelChoiceField(queryset= sorted(Especie.objects.all(), key=lambda x : x.nome),
+            self.fields['especie'] = ModelChoiceField(queryset=Especie.objects.all().order_by('nome'),
                                                       to_field_name='nome',
                                                       empty_label="Selecione a Espécie")
