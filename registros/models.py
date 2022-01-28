82f5a30c445872b39c6ffd0fec49bd5c40b4117b
taniagmangolini <taniagmangolini@gmail.com>
2022-01-27 23:04:59 -0300

    add constants.py

--- registros/models.py
+++ registros/models.py
@@ -1,63 +1,26 @@
 from django.db import models
-import uuid # Required for unique book instances
+import uuid
 from especies.models import Especie
 from formacaoflorestal.models import FormacaoFlorestal
+from suceco._constants import ESTAGIOS, STATUS, ESTADOS
 
 class Registro(models.Model) :
-    ESTAGIOS = (
-        ('Pioneira', 'Pioneira'),
-        ('Secundária Inicial', 'Secundária Inicial'),
-        ('Secundária Tardia', 'Secundária Tardia'),
-        ('Umbrófila', 'Umbrófila'),
-        ('Secundária', 'Secundária'),
-        ('Climácica', 'Climácica')
-    )
-
-    STATUS = (
-        ('Ativo', 'Ativo'),
-        ('Inativo', 'Inativo')
-    )
-
-    ESTADOS = (
-        ('AC', 'AC'),
-        ('AL', 'AL'),
-        ('AP', 'AP'),
-        ('AM', 'AM'),
-        ('BA', 'BA'),
-        ('CE', 'CE'),
-        ('DF', 'DF'),
-        ('ES', 'ES'),
-        ('GO', 'GO'),
-        ('MA', 'MA'),
-        ('MG', 'MG'),
-        ('MT', 'MT'),
-        ('MS', 'MS'),
-        ('PA', 'PA'),
-        ('PE', 'PE'),
-        ('PB', 'PB'),
-        ('PI', 'PI'),
-        ('PR', 'PR'),
-        ('RJ', 'RJ'),
-        ('RN', 'RN'),
-        ('RR', 'RR'),
-        ('RS', 'RS'),
-        ('RO', 'RO'),
-        ('SC', 'SC'),
-        ('SE', 'SE'),
-        ('SP', 'SP'),
-        ('TO', 'TO')
-    )
 
     # Primary
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
 
     # Foreign Keys
     especie = models.ForeignKey(Especie, on_delete=models.CASCADE)
-    estagio = models.CharField(max_length=50, choices=ESTAGIOS)
     formacao_florestal = models.ForeignKey(FormacaoFlorestal, on_delete=models.SET_NULL, null=True)
+
+    # Other fields
+    estagio = models.CharField(max_length=50, choices=ESTAGIOS)
     estado = models.CharField(max_length=50, choices=ESTADOS)
     status = models.CharField(max_length=15, choices=STATUS, default='Ativo')
     referencia = models.CharField(max_length=1000, default='')
     detalhes = models.CharField(max_length=1000, default= '')
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return '-'.join([str(self.id), self.especie.nome, self.formacao_florestal.nome])
