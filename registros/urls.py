6a1243ed8ebe2a1dd4fe5b4b60d28e0ef319aad1
taniagmangolini <taniagmangolini@gmail.com>
2022-01-27 22:37:30 -0300

    refatoring

--- registros/urls.py
+++ registros/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.RegistrosView.as_view(), name='index'),
-    #path('', views.search_especies, name='search_especies'),
     path('', views.index, name='reg_index'),
     path('search/', views.search, name='search_regs_by_sp'),
     path('edit/(?P<id>\d+)$/', views.edit, name='edit_registro'),
     path('create/', views.create, name='create_registro'),
-    path('createlote/', views.create_lote, name='create_lote'),
     path('delete/(?P<id>\d+)$/', views.delete, name='delete_registro')
 ]
--- registros/views.py
+++ registros/views.py
@@ -14,6 +14,10 @@ logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def index(request) :
+    '''
+    :param request:
+    :return: main page
+    '''
     especies_list =   sorted(  Especie.objects.all(), key=lambda x : x.nome)
     contexto = {
         'especies_list' : especies_list,
@@ -24,8 +28,14 @@ def index(request) :
     }
     return render(request, 'registros/list.html', contexto)
 
+
 @csrf_exempt
 def search(request, id=None):
+    '''
+    :param request:
+    :param id:
+    :return: list of registers for the selected species
+    '''
     especies_list = sorted(Especie.objects.all(), key=lambda x : x.nome)
     especie_selected = None
     template_name = 'registros/list.html'
@@ -43,13 +53,16 @@ def search(request, id=None):
         'especie_selected' : especie_selected,
         'nr_registros' : len(registros)
     }
-
     return render(request, template_name , contexto )
+
 
 @login_required
 def create(request):
+    '''
+    :param request:
+    :return: a new register
+    '''
     form = RegistroForm()
-    logger.info("Lote:")
     logger.info(request.POST.get('dados_lote', None))
     try:
         if request.method == 'POST':
@@ -59,118 +72,61 @@ def create(request):
                 form.save()
                 return HttpResponseRedirect('/registros/')
             else:
-                logger.info( form.errors.as_text())
+                logger.info(form.errors.as_text())
                 return render(request, 'registros/create.html', {'form' : form})
         else:
             return render(request, 'registros/create.html', {'form' : form})
-    except:
+
+    except Exception as e:
         logger.info(form)
-        logger.info("=>>>> Erro ao criar registros")
+        logger.info("[ERROR] creating register")
         return render(request, 'registros/create.html', {'form' : form})
-
-@login_required
-def create_lote(request):
-    form = RegistroLoteForm()
-    registro = Registro()
-    dados_lote =  request.POST.get('dados_lote', '')
-    vegetacao_nome =  request.POST.get('formacao_florestal', '')
-    estado = request.POST.get('estado', '')
-    referencia = request.POST.get('referencia', '')
-    detalhes = request.POST.get('detalhes', '')
-    especies_estagios = []
-
-    try :
-        if dados_lote == '' :
-            return render(request, 'registros/create_lote.html', {'form' : form})
-        else:
-
-            try :
-                vegetacao = FormacaoFlorestal.objects.get(nome=vegetacao_nome)
-                logger.info('formacao: ' + vegetacao.nome)
-            except FormacaoFlorestal.DoesNotExist :
-                vegetacao = None
-
-            especies_estagios = str.split(dados_lote, ',')
-            logger.info(especies_estagios)
-
-            for i in especies_estagios:
-                logger.info(i)
-                especie = None
-                especie_estagio = str.split(i, ':')
-                estagio = ''
-                especie_nome = ''
-
-                try:
-                    logger.info(especie_estagio[0])
-                    logger.info(especie_estagio[1])
-                    especie_nome = especie_estagio[0].strip()
-                    estagio = especie_estagio[1].strip()
-                except:
-                    logger.info('split error')
-
-                if  estagio != '' and especie_nome != '':
-                    try:
-                        especie  =  Especie.objects.get(nome=especie_nome)
-                        logger.info('especie: ' + especie.nome)
-                    except:
-                        if (especie == None):
-                            new_especie = Especie()
-                            new_especie.nome =  especie_nome
-                            new_especie.save()
-                            logger.info('especie add: ' +  especie_nome)
-                            especie = Especie.objects.get(nome=especie_nome)
-
-                logger.info('especie: ' + especie.nome)
-                logger.info('vegetacao: ' + vegetacao.nome)
-
-                if(especie != None and vegetacao != None):
-                    logger.info('creating registro')
-                    registro = Registro()
-                    registro.especie = especie
-                    registro.estagio = estagio
-                    registro.formacao_florestal = vegetacao
-                    registro.estado = estado
-                    registro.referencia = referencia
-                    registro.detalhes = detalhes
-                    logger.info('saving')
-                    registro.save()
-                    logger.info('saved')
-
-        return HttpResponseRedirect('/registros/')
-    except:
-        logger.info("=>>>> Erro ao criar registros")
-        return render(request, 'registros/create_lote.html', {'form' : form})
-
 
 
 @login_required
 def edit(request, id, template_name='registros/edit.html') :
-        form = RegistroForm()
-        logger.info('>>>edit registros: ' + id)
-        try :
-            registro = get_object_or_404(Registro, id=id)
-            logger.info(registro)
-            form = RegistroForm(request.POST or None, instance=registro)
-            logger.info(form.is_valid())
-            if form.is_valid() :
-                form.save()
-                return HttpResponseRedirect('/registros/')
-            else :
-                logger.info(form.errors.as_text())
-                return render(request, template_name, {'form' : form})
-        except :
-            logger.info(form)
-            logger.info("=>>>> Erro ao editar registros")
-            return render(request, template_name, {'form' : form})
+    '''
+    :param request:
+    :param id:
+    :param template_name:
+    :return: form with the changed register
+    '''
+    form = RegistroForm()
+    logger.info('editing register' + id)
+    try:
+        registro = get_object_or_404(Registro, id=id)
+        logger.info(registro)
+        form = RegistroForm(request.POST or None, instance=registro)
+        logger.info(form.is_valid())
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/registros/')
+        else:
+            logger.info(form.errors.as_text())
+            return render(request, template_name, {'form': form})
+
+    except Exception as e:
+        logger.info(form)
+        logger.info("[ERROR] editing register...")
+        logger.info(str(e))
+        return render(request, template_name, {'form': form})
+
 
 @login_required
-def delete(request, id, template_name='registros/delete.html') :
-        logger.info('delete registros: ' + id)
-        try :
-            registro = get_object_or_404(Registro, id=id)
-            registro.delete()
-            logger.info('deletado!')
-            return HttpResponseRedirect('/registros/')
-        except :
-            logger.info("=>>>> Erro ao deletar registro " + id)
-            return render(request, template_name)
+def delete(request, id, template_name='registros/delete.html'):
+    '''
+    :param request:
+    :param id:
+    :param template_name:
+    :return: registers page
+    '''
+    try:
+        logger.info("Deleting " + id)
+        registro = get_object_or_404(Registro, id=id)
+        registro.delete()
+        return HttpResponseRedirect('/registros/')
+
+    except Exception as e:
+        logger.info("[ERROR] deleting register")
+        return render(request, template_name)
