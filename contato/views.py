0d614df7bc278e5b744a90d29c9d63f117677eff
taniagmangolini <taniagmangolini@gmail.com>
2022-01-30 01:08:48 -0300

    changing mail service

--- contato/views.py
+++ contato/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
 from .forms import ContatoForm
 from django.shortcuts import render, redirect
-from django.core.mail import send_mail
 from suceco import settings
+from sendgrid.helpers.mail import Mail
+from sendgrid import SendGridAPIClient
 import logging
 
 logger = logging.getLogger(__name__)
 
-def mandar_mensagem(request):
+
+def mandar_mensagem_old(request):
     form = ContatoForm()
     template_name = 'contato/contato.html'
     contexto = {'form' : form  }
@@ -28,27 +29,27 @@ def mandar_mensagem(request):
             logger.info('EMAIL_PORT ' +  str(settings.EMAIL_PORT))
             logger.info('EMAIL_HOST_PASSWORD ' +  settings.EMAIL_HOST_PASSWORD)
 
-            send_mail(
-                assunto,
-                (mensagem + '. Email contato: ' + email_contato),
-                settings.EMAIL_HOST_USER,
-                [settings.EMAIL_DESTINATARIO],
-                fail_silently=False,
-            )
+            message = Mail(
+                from_email=settings.EMAIL_HOST_USER,
+                to_emails=settings.EMAIL_HOST_USER,
+                subject='SUCECO',
+                html_content= mensagem + '. Email contato: ' + email_contato)
 
-            logger.info('msg sent ')
-            contexto = {
-                'form' : form,
-                'msg' : 'Mensagem enviada',
-            }
+            try:
+                sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+                response = sg.send(message)
+                logger.info('msg sent ')
+                contexto = {
+                    'form' : form,
+                    'msg' : 'Mensagem enviada',
+                }
 
-        return render(request, template_name, contexto)
+            except Exception as e:
+                logger.info('[ERRO] sending email')
+                logger.error(e)
+                contexto = {
+                    'form' : form,
+                    'msg' : 'Não foi possível enviar a enviar mensagem.',
+                }
 
-    except Exception as e:
-        logger.info('[ERRO] sending email')
-        logger.error(e)
-        contexto = {
-            'form' : form,
-            'msg' : 'Não foi possível enviar a enviar mensagem.',
-        }
         return render(request, template_name, contexto)
